13dd1af6fd2009a1d3eb99a776f480d936bcb31b
Harsh17380 <106827228+Harsh17380@users.noreply.github.com>
2023-06-09 12:44:23 +0530

    Update P1_Start_Square.java

--- src/P1_Start_Square.java
+++ src/P1_Start_Square.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class P1_Start_Square {
+public class P1_Start_Square {
+    public static void main(String[] args) {
+        int row = 5 ;
+        rows(row,row);
+    }
+
+    public static void rows(int row,int n) {
+        if (row == 0){
+            return ;
+        }
+        stars(n);
+        System.out.println();
+        rows(row-1,n);
+    }
+
+    public static void stars(int n) {
+        if (n==0){
+            return;
+        }
+        System.out.print("*");
+        stars(n-1);
+    }
 }
